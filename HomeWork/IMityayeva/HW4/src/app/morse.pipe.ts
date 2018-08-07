ef83f2758ac1ab27ad01575c5c85e9c3bf85bcd7
IMityayeva <irina.mityayeva.92@gmail.com>
2018-08-07 07:11:03 +0300

    Homework 4 pipe

---
+++ HomeWork/IMityayeva/HW4/src/app/morse.pipe.ts
@@ -0,0 +1,55 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+const morseMap = {
+  "a": ". _",
+  "b": "_ . . .",
+  "c": "_ . _ .",
+  "d": "_ . .",
+  "e": ".",
+  "f": ". . _ .",
+  "g": "_ _ .",
+  "h": ". . . .",
+  "i": ". .",
+  "j": ". _ _ _",
+  "k": "_ . _",
+  "l": ". _ . .",
+  "m": "_ _",
+  "n": "_ .",
+  "o": "_ _ _",
+  "p": ". _ _ .",
+  "q": "_ _ . _",
+  "r": ". _ .",
+  "s": ". . .",
+  "t": "_",
+  "u": ". . _",
+  "v": ". . . _",
+  "w": ". _ _",
+  "x": "_ . . _",
+  "y": "_ . _ _",
+  "z": "_ _ . .",
+  "0": "_ _ _ _ _",
+  "1": ". _ _ _ _",
+  "2": ". . _ _ _",
+  "3": ". . . _ _",
+  "4": ". . . . _",
+  "5": ". . . . .",
+  "6": "_ . . . .",
+  "7": "_ _ . . .",
+  "8": "_ _ _ . .",
+  "9": "_ _ _ _ ."
+};
+
+@Pipe({
+  name: 'morse'
+})
+export class MorsePipe implements PipeTransform {
+  transform(value: string): string {
+    if (/[a-zA-Z0-9]+/.test(value)) {
+      return "["+ value.toLowerCase().split("").map(symbol => {
+        return morseMap[symbol] || symbol;
+      }).join("][") + "]";
+    } else {
+      return "";
+    }
+  }
+}
