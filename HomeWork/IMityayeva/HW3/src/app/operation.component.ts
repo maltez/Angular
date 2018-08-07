dc0444b528687720bf533c5370a67273756e220b
IMityayeva <irina.mityayeva.92@gmail.com>
2018-08-07 06:47:43 +0300

    Homework 3 completed

--- HomeWork/IMityayeva/HW3/src/app/operation.component.ts
+++ HomeWork/IMityayeva/HW3/src/app/operation.component.ts
@@ -2,7 +2,7 @@ import {Component, Input, Output, EventEmitter} from "@angular/core";
 
 @Component({
   selector: 'operation-button',
-  template: `<button type="button" (click)="passOperation($event)" class="btn btn-secondary btn-lg">{{ operationValue }}</button>`
+  template: `<button type="button" (click)="passOperation($event)" class="btn btn-secondary btn-lg" style="width: 48px;">{{ operationValue }}</button>`
 })
 
 export class OperationComponent {
